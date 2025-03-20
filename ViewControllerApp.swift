bdf2109899704d438e291acc3c2d98254e69c92b
Alamjeet Singh <53936148+supermogaboy@users.noreply.github.com>
2025-03-20 14:17:18 -0400

    Add files via upload

---
+++ ViewControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerApp.swift
+//  ViewController
+//
+//  Created by Alam on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
